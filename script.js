62625bed926939fd9a24622422e4bb6d6a2cae7c
KWacker <117930676+KWacker0@users.noreply.github.com>
2022-11-17 19:47:12 +0000

    Bug Fixes

--- script.js
+++ script.js
@@ -13,18 +13,19 @@ document.getElementById("scissorsIMG").addEventListener("click" , begin);
 document.getElementById("tryAgain").addEventListener("click" , begin);
 document.getElementById("restart").addEventListener("click" , begin);
 
-
-
 comNum = Math.floor((Math.random()*3)+1);
     
 
 function begin(e) {
+
+  // Start
   if(e.target.id === "start") {
     document.getElementById("rulesBlock").className = "display-none";
     document.getElementById("gameBtns").className = "row margin-auto";
     document.getElementById("rockIMG").className = "img-fluid mx-auto d-inline-block centered margin-auto";
     document.getElementById("paperIMG").className = "img-fluid mx-auto d-inline-block centered margin-auto";
     document.getElementById("scissorsIMG").className = "img-fluid mx-auto d-inline-block centered margin-auto";
+    
   }
   
 
@@ -35,14 +36,14 @@ function begin(e) {
       if(remAttem === 0) {
         document.getElementById("gameBtns").className = "display-none";
         document.getElementById("restScrn").className = "text-center bg-light-blue col-4 centered vertical-align";
-        document.getElementById("restMess").innerHTML = `Would You Like To play Again you won ${winCount} times out of 5 and lost            ${lossCount} times out of 5`;
+        document.getElementById("restMess").innerHTML = `Would you like to play again? You won ${winCount} times out of 5 and lost ${lossCount} times out of 5`;
       }else
       if(comNum === 2) {
         winCount++
         document.getElementById("gameBtns").className = "display-none";
         document.getElementById("winLossScrn").className = "text-center bg-light-blue col-4 centered vertical-align";
         document.getElementById("winLoss").innerHTML = `Congrats! You Won!`;
-        document.getElementById("winLossCount").innerHTML = `${usrChoice} beats Scissors you have ${remAttem} more trys to secure the victory`;
+        document.getElementById("winLossCount").innerHTML = `${usrChoice} beats Scissors you have ${remAttem} more trys to secure the victory!`;
         document.getElementById("tryAgain").innerHTML = `Continue`;
       }else if(comNum === 1) {
         remAttem++
@@ -67,14 +68,14 @@ function begin(e) {
       if(remAttem === 0) {
         document.getElementById("gameBtns").className = "display-none";
         document.getElementById("restScrn").className = "text-center bg-light-blue col-4 centered vertical-align";
-        document.getElementById("restMess").innerHTML = `Would You Like To play Again you won ${winCount} times out of 5 and lost            ${lossCount} times out of 5`;
+        document.getElementById("restMess").innerHTML = `Would you like to play again? You won ${winCount} times out of 5 and lost ${lossCount} times out of 5`;
       }else
       if(comNum === 1) {
         winCount++
         document.getElementById("gameBtns").className = "display-none";
         document.getElementById("winLossScrn").className = "text-center bg-light-blue col-4 centered vertical-align";
         document.getElementById("winLoss").innerHTML = `Congrats! You Won!`;
-        document.getElementById("winLossCount").innerHTML = `${usrChoice} beats Rock you have ${remAttem} more trys to secure the victory`;
+        document.getElementById("winLossCount").innerHTML = `${usrChoice} beats Rock you have ${remAttem} more trys to secure the victory!`;
         document.getElementById("tryAgain").innerHTML = `Continue`;
       }else if(comNum === 2) {
         remAttem++
@@ -99,14 +100,14 @@ function begin(e) {
       if(remAttem === 0) {
         document.getElementById("gameBtns").className = "display-none";
         document.getElementById("restScrn").className = "text-center bg-light-blue col-4 centered vertical-align";
-        document.getElementById("restMess").innerHTML = `Would You Like To play Again you won ${winCount} times out of 5 and lost            ${lossCount} times out of 5`;
+        document.getElementById("restMess").innerHTML = `Would you like to play again? you won ${winCount} times out of 5 and lost ${lossCount} times out of 5`;
       }else
       if(comNum === 2) {
         winCount++
         document.getElementById("gameBtns").className = "display-none";
         document.getElementById("winLossScrn").className = "text-center bg-light-blue col-4 centered vertical-align";
         document.getElementById("winLoss").innerHTML = `Congrats! You Won!`;
-        document.getElementById("winLossCount").innerHTML = `${usrChoice} beats Paper you have ${remAttem} more trys to secure the victory`;
+        document.getElementById("winLossCount").innerHTML = `${usrChoice} beats Paper you have ${remAttem} more trys to secure the victory!`;
         document.getElementById("tryAgain").innerHTML = `Continue`;
       }else if(comNum === 3) {
         remAttem++
